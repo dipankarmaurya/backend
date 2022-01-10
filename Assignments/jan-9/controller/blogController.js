77c3bd98f890abe523639ef8cc7e689d278ea0bd
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2022-01-11 00:21:42 +0530

    done sheet updation (file system)

--- Assignments/jan-9/controller/blogController.js
+++ Assignments/jan-9/controller/blogController.js
@@ -1,6 +1,21 @@
 // const { arrayBuffer } = require('stream/consumers');
 const blogs = require('../model/blog');
-console.log()
+const fs = require('fs');
+
+// updating blog model after updating and deleting 
+function upDateSheet(blogs) {
+
+    fs.writeFile("../jan-9/model/blog.js", "var blogs="+JSON.stringify(blogs)+ "\nmodule.exports=blogs ", (err) =>{
+        if (err) {
+            console.log(err)
+            return {"error": err}
+        } else {
+            console.log(
+                'file updated booking'
+            )
+        }
+    })
+}
 function findById(id) {
     let finedBlog;
     blogs.forEach((blog) => {
@@ -9,17 +24,24 @@ function findById(id) {
             finedBlog= blog;
         }
     })
+    
     return finedBlog;
 }
 
 function findByIdAndUpdateTitle(id,title) {
 let blog = findById(id);
-blog.title=title;
+    blog.title = title;
+    // console.log(blogs);
+    upDateSheet(blogs)
     return blog;
+
 }
 
 function findByIdAndDelete(id) {
-   return  blogs.filter(blog => blog.id != id);
+    let newBlogs = blogs.filter(blog => blog.id != id);
+    upDateSheet(newBlogs);
+    return newBlogs;
+    
 }
 
 module.exports = {
